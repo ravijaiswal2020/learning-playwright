e349ab7c8c693a836e74c35b3afb2a62e1ed8361
Sehejravi <ravisweetsehej123@gmail.com>
2026-02-23 12:01:13 +0530

    tools updated

--- tests/tool.spec.js
+++ tests/tool.spec.js
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('login with valid cred and research explore', async ({ page }) => {
 
-  await page.goto('https://dev.taxmann.com/gp/auth/login');
+  await page.goto('https://taxmann.com/gp/auth/login');
 
   await page.getByRole('button', { name: 'Login with Email' }).click();
   await page.getByRole('textbox', { name: 'Enter Email' }).fill('cog.testing2021+282@gmail.com');
@@ -45,7 +45,7 @@ await page.getByRole('link', { name: 'Calculate' }).click();
 
 
 //add await 
-await page.waitForTimeout(2000);
+//await page.waitForTimeout(2000);
 //await page.pause
 
  
